94b6a6a33250a813e1a878c5fbbf00796c44d332
Fat ^_^ <Fat ^_^>
2016-10-07 11:19:43 -0700

    test

--- gargarF/app/src/main/java/com/example/fatimagargar/gargaf/OnTouchActivity.java
+++ gargarF/app/src/main/java/com/example/fatimagargar/gargaf/OnTouchActivity.java
@@ -1,54 +1,101 @@
 package com.example.fatimagargar.gargaf;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
+
 /**
- * Created by Fatima Gargar on 10/4/2016.
+ * Created by androidstudio on 10/09/16.
  */
-public class OnTouchActivity extends Activity {
+public class OnTouchActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_touch);
 
-        final ImageView image = (ImageView) findViewById(R.id.image);
+        final ImageView image = (ImageView)findViewById(R.id.image);
+        final EditText text =(EditText) findViewById(R.id.Text1);
+        final EditText text1 =(EditText) findViewById(R.id.Text3);
+        final EditText text2 =(EditText) findViewById(R.id.Text4);
+        final EditText text3 =(EditText) findViewById(R.id.Text);
+        final EditText text4 =(EditText) findViewById(R.id.Text5);
 
-        image.setOnTouchListener(new View.OnTouchListener() {
-            float inX, finX, inY, finY;
 
+        image.setOnTouchListener(new View.OnTouchListener(){
+            float x1,x2,y1,y2,diffy1,diffx1,diffx,diffy;
             @Override
-            public boolean onTouch(View v, MotionEvent motionEvent) {
-                int e = motionEvent.getAction();
-                switch (e) {
+            public boolean onTouch(View v,MotionEvent motion){
+                int a= motion.getAction();
+                switch(a){
                     case MotionEvent.ACTION_DOWN:
-                        inX = motionEvent.getX();
-                        inY = motionEvent.getY();
-                        Toast.makeText(getApplicationContext(), "X= " + inX + " Y= " + inY, Toast.LENGTH_SHORT).show();
+                        x1=motion.getX();
+                        y1=motion.getY();
+                        text.setText("X1 " + x1 + " Y1 " + y1);
                         return true;
                     case MotionEvent.ACTION_UP:
-                        finX = motionEvent.getX();
-                        finY = motionEvent.getY();
-                        if (inX < finX) {
-                            Toast.makeText(getApplicationContext(), "X= " + finX + "Y= " + finY + " Swipe Right", Toast.LENGTH_SHORT).show();
+                        x2=motion.getX();
+                        y2= motion.getY();
+                        text1.setText("X2 " + x2 + " Y2 " + y2);
+                        if (x2>x1 && y2>y1) {
+                            diffx = x2 - x1;
+                            diffy = y2 - y1;
+                            text2.setText("X= " + diffx + " Y= " + diffy);
                         }
-                        if (inX > finX) {
-                            Toast.makeText(getApplicationContext(), "X= " + finX + "Y= " + finY + " Swipe Left", Toast.LENGTH_SHORT).show();
+                        if (x1>x2 && y1>y2){
+                            diffx1=x1-x2;
+                            diffy1=y1-y2;
+                            text2.setText("X= " + diffx1 + " Y= " + diffy1);
                         }
-                        if (inY < finY) {
-                            Toast.makeText(getApplicationContext(), "X= " + finX + "Y= " + finY + " Swipe Down", Toast.LENGTH_SHORT).show();
+                        if (x1<x2 && y1<y2){
+                            text3.setText("Swipe Right and Down");
                         }
-                        if (inY > finY) {
-                            Toast.makeText(getApplicationContext(), "X= " + finX + "Y= " + finY + " Swipe Up", Toast.LENGTH_SHORT).show();
+                        if (x1>x2 && y1>y2){
+                            text3.setText("Swipe Left and Up");
+
+                        }
+                        if (x1<x2 && y1>y2){
+                            text3.setText("Swipe Right and Up");
+                        }
+                        if (x1>x2 && y1<y2){
+                            text3.setText("Swipe Left and Down");
+                        }
+                        if (diffx<0 && diffy<0){
+                            text4.setText("Quadrant 3");
+                        }
+                        if (diffx>0 && diffy<0){
+                            text4.setText("Quadrant 4");
+                        }
+                        if (diffx>0 && diffy>0){
+                            text4.setText("Quadrant 1");
+                        }
+                        if (diffx<0 && diffy>0){
+                            text4.setText("Quadrant 2");
+                        }
+                        if (diffx1<0 && diffy1<0){
+                            text4.setText("Quadrant 3");
+                        }
+                        if (diffx1>0 && diffy1<0){
+                            text4.setText("Quadrant 4");
+                        }
+                        if (diffx1>0 && diffy1>0){
+                            text4.setText("Quadrant 1");
+                        }
+                        if (diffx1<0 && diffy1>0){
+                            text4.setText("Quadrant 2");
                         }
                         return true;
 
                 }
                 return false;
+
             }
+
         });
     }
+
+
 }
